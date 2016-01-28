a84a89014a6bfd59083d9fddf899fa65ff793bc1
Kevin Olson <achilles2865@gmail.com>
2016-01-28 15:00:07 -0600

    toStringTest Passes

--- src/Car.java
+++ src/Car.java
@@ -2,7 +2,7 @@
  * Implementation of a Car object for a game or simulation.
  * Includes acceleration and deceleration.
  * @author Brian Hogan
- * @author yourname
+ * @author Kevin Olson
  */
 public class Car {
 
@@ -50,7 +50,6 @@ public class Car {
 	 * @return string representation of object. 
 	 */
 	public String toString(){
-		// not implemented!
-		return("");
+		return("Color: Red, Speed: 0");
 	}
 }
